2e63bcc58bc61468f9444e1ff8996c3f489e90b5
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-08-30 12:32:18 -0400

    Update app.d

--- source/app.d
+++ source/app.d
@@ -1,5 +1,9 @@
 import raylib_misc;
 import std.string;
+import std.array;
+import std.stdio;
+import std.conv;
+import std.uni;
 
 enum maxSound = 5,
      minSound = 0,
@@ -14,28 +18,54 @@ ubyte memoryPointer,
       registerA,
       registerB;
         
-auto volume = 0f;
+auto volume = 0f, debugging = false;
         
 ubyte[memSize] memory;
 
 const glyphs = 
 [  
-  '👋', // play
-  '✋', // end if/repeat
-  '👌', // repeat
-  '✌', // store to volume
-  '🤘', // store to sound type
-  '👈', // move + 1
-  '👉', // move - 1
-  '👆', // increment
-  '👇', // decrement
-  '👍', // higher than
-  '👎', // lower than
-  '✊', // store to register a
-  '👊', // store to register b
-  '🤛', // from register a
-  '🤜'  // from register b
+  "👋", // play
+  "✋", // end if/repeat
+  "👌", // repeat
+  "✌", // store to volume
+  "🤘", // store to sound type
+  "👈", // move + 1
+  "👉", // move - 1
+  "👆", // increment
+  "👇", // decrement
+  "👍", // higher than
+  "👎", // lower than
+  "✊", // store to register a
+  "👊", // store to register b
+  "🤛", // from register a
+  "🤜"  // from register b
 ];
+
+void read(string src)
+{
+    auto gsrc = src.byGrapheme.array;
+    for (size_t i, lineCount = 1, columnCount = 1; i < gsrc.length; ++i, ++columnCount)
+    {
+        auto current = gsrc[i].array.byCodePoint.text;
+        
+        switch (current)
+        {
+            case " ": continue;
+            
+            case "\n":
+                columnCount = 0;
+                ++lineCount;
+                break;
+                
+            case "👋": 
+            writeln("Reached here"); 
+            break;
+            
+            default:
+            break;
+        }
+    }
+}
 
 void main(string[] args)
 {
@@ -50,4 +80,6 @@ void main(string[] args)
         new Snd(noisePath.format("scratch")),
         new Snd(noisePath.format("slap"))
     ];
+    
+    read("sdfs👋");
 }
